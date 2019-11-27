02659638fdce05edf65624c05da0176b77d1ee58
Zac Mitton <zacmitton22@gmail.com>
2019-11-27 16:44:45 -0400

    wip

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 window.urlsafe = require('url-encode-decode')
 
 window.render = () => {
-  let h1 = document.getElementById('h1')
   let a1 = document.getElementById('a1')
+  let a2 = document.getElementById('a2')
   let e1 = document.getElementById('e1')
   let d1 = document.getElementById('d1')
   let l1 = document.getElementById('l1')
   let b1 = document.getElementById('b1')
-  e1.innerHTML =  h1.innerHTML + window.urlsafe.encode(a1.value)
+  e1.innerHTML =  a1.value + window.urlsafe.encode(a2.value)
   d1.innerHTML =urlsafe.decode(e1.innerHTML)
   l1.setAttribute('href', e1.innerHTML)
 
-  // let l2 = document.getElementById('link2')
+  let l2 = document.getElementById('link2')
+
   console.log("rendering area 1")
 
 }
+window.invoke = () => {
+  render()
+  window.location = e1.innerHTML
+}
+
+window.onload(function(){
+  render()
+})
